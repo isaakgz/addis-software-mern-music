1581e74447fb6ef4cc1eea68d3e5485bf0ab825a
yesak <yesakgz12@gmail.com>
2024-08-26 14:36:14 +0300

    Initialize Express server with dotenv configuration

---
+++ Server/app.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+import express from "express";
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
